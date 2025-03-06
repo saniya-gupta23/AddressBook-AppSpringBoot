f60d186b53f0747334a643c07841b879079ebbf3
Saniya <saniya.gupta_cs21@gla.ac.in>
2025-03-06 15:04:47 +0530

    Sprring App  SetUp

---
+++ src/test/java/com/BridgeLabz/AddressBook/App/AddressBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BridgeLabz.AddressBook.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
