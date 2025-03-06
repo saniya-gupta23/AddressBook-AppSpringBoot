56095006d388e2dc8b74f0526b2f045e598333df
Saniya <saniya.gupta_cs21@gla.ac.in>
2025-03-06 16:19:18 +0530

    Added:DTO & MODEL Layer

---
+++ src/main/java/com/BridgeLabz/AddressBook/App/DTO/AddressBookDTO.java
@@ -0,0 +1,11 @@
+package com.BridgeLabz.AddressBook.App.DTO;
+
+
+import lombok.Data;
+
+@Data
+public class AddressBookDTO {
+    private String name;
+    private String phone;
+    private String address;
+}
